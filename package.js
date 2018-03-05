48e608d3ce47e03c54d33c5a7f305388ea4bed64
Aram Kocharyan <akocharyan@fb.com>
2018-03-04 21:31:54 -0800

    Update version

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'aramk:google-maps-marker-label',
   summary: 'MarkerWithLabel for Google Maps API',
-  version: '1.1.10'
+  version: '2.0.0'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom(['METEOR@0.9.0']);
+  api.versionsFrom(['METEOR@1.6.1']);
   api.use([
     'mrt:googlemaps@0.1.1'
   ]);
